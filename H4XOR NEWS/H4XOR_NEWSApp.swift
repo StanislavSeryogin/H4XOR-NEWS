7baa3f20817c97e038e1c2eaee3ff0f75bc8af48
Stanislav <stas.seryogin.87@gmail.com>
2023-08-18 10:02:59 +0300

    Initial Commit

---
+++ H4XOR NEWS/H4XOR_NEWSApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4XOR_NEWSApp.swift
+//  H4XOR NEWS
+//
+//  Created by Stanislav Seryogin on 18.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct H4XOR_NEWSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
